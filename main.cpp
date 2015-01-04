b7a5ef7f060f01f01c10d94e026b4c1047db6706
Petschko <peter-91@hotmail.de>
2015-01-05 00:31:23 +0100

    test

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 // test
 // test 2
 // test2
+// test 3
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK ChildProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK CheckRadioProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
