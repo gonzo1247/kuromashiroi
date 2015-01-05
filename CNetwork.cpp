c9d99cfabe1ba3bbacfcb449fd5fccf9602a3bb2
sevi <gonzo1247@yahoo.de>
2015-01-05 23:39:14 +0100

    Client/Network: Begin coding for the client <> server connection.

---
+++ CNetwork.cpp
@@ -0,0 +1,14 @@
+#include "CNetwork.h"
+
+
+
+CNetwork::~CNetwork()
+{
+    
+}
+
+void CNetwork::CreateConnect()
+{
+  //  SOCKET socket(int af, int type, int protocol);
+
+}
---
+++ CNetwork.h
@@ -0,0 +1,40 @@
+﻿#ifndef __CNETWORK
+#define __CNETWORK
+
+#include <stdio.h>
+#include <errno.h>
+
+#ifdef _WIN32
+/* Headerfiles für Windows */
+#include <winsock.h>
+#include <io.h>
+#define in_addr_t unsigned long
+
+#else
+/* Headerfiles für Unix/Linux */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#define closesocket(s) close(s)
+#endif
+
+class CNetwork
+{
+    public:
+        CNetwork();
+        ~CNetwork();
+        void CreateConnect();
+
+};
+
+
+
+
+
+
+
+
+#endif __CNETWORK
